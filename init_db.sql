c20a22c0e32816ae6f081d10043463c58c1ced94
Mate Antunovic <just.mate.antunovic@gmail.com>
2017-12-01 12:43:16 -0500

    almost there

--- init_db.sql
+++ init_db.sql
@@ -3,14 +3,14 @@ INSERT INTO accounting_period (from_date, to_date) VALUES ('2017-12-01', '2017-1
 
 SELECT open_credit_normal_account(
     'nzd',
-    'cent'
+    'cent',
     'just.mate.antunovic@gmail.com'
 );
 
 SELECT open_credit_normal_account(
     'nzd',
-    'cent'
+    'cent',
     'mate@antunovic.nz'
 );
 
-SELECT transfer_asset('nzd', 'cents', '2017-12-01', '2017-12-31', 0, 1, 100);
+SELECT transfer_asset('nzd', 'cent', '2017-12-01', '2017-12-31', 0, 1, 100);
--- procedures.sql
+++ procedures.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE FUNCTION transfer_asset(
-    asset_code VARCHAR(4),
-    asset_denom VARCHAR(6),
+    asset_code_var VARCHAR(4),
+    asset_denom_var VARCHAR(6),
     account_period_start DATE,
     account_period_end DATE,
     debit_account INTEGER,
@@ -13,11 +13,13 @@ correspondence_id INTEGER;
 asset_type_id INTEGER;
 account_period_id INTEGER;
 BEGIN
-    IF NOT EXISTS (SELECT id INTO asset_type_id FROM asset_type WHERE asset_code = asset_code AND asset_denom = asset_denom LIMIT 1) THEN
+    SELECT id INTO asset_type_id FROM asset_type WHERE asset_code = asset_code_var AND denom = asset_denom_var LIMIT 1;
+    IF asset_type_id IS NULL THEN
         RAISE EXCEPTION 'Cannot move asset with unknown asset type';
     END IF;
-    IF NOT EXISTS (SELECT id INTO account_period_id FROM accounting_period WHERE account_period_start = from_date AND account_period_start = end_date LIMIT 1) THEN
-        RAISE EXCEPTION 'Cannot move asset with unknown asset type';
+    SELECT id INTO account_period_id FROM accounting_period WHERE account_period_start = from_date AND account_period_start = to_date LIMIT 1;
+    IF account_period_id IS NULL THEN
+        RAISE EXCEPTION 'Cannot move asset with unknown asset type (% in %s)', asset_code_var, asset_denom_var;
     END IF;
     correspondence_id := (SELECT COUNT(*) FROM JOURNAL);
     INSERT INTO journal(accounting_period, account_id, asset_type_id, correspondence_id, credit, debit)
@@ -28,8 +30,8 @@ END;
 $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION open_credit_normal_account(
-    asset_code VARCHAR(4),
-    asset_denom VARCHAR(4),
+    asset_code_var VARCHAR(4),
+    asset_denom_var VARCHAR(4),
     owner_email VARCHAR(256)
 ) RETURNS VOID AS
 $$
@@ -37,12 +39,15 @@ DECLARE
 owner_id INTEGER;
 asset_type_id INTEGER;
 BEGIN
-    IF NOT EXISTS(SELECT id INTO owner_id FROM account_owner WHERE email_address = owner_email LIMIT 1) THEN
-        INSERT INTO account_owner VALUES (owner_email);
+    SELECT id INTO owner_id FROM account_owner WHERE email_address = owner_email LIMIT 1;
+    IF owner_id IS NULL THEN
+        INSERT INTO account_owner(email_address) VALUES (owner_email);
+        SELECT id INTO owner_id FROM account_owner WHERE email_address = owner_email LIMIT 1;
     END IF;
-    IF NOT EXISTS (SELECT id INTO asset_type_id FROM asset_type WHERE asset_code = asset_code AND asset_denom = asset_denom LIMIT 1) THEN
+    SELECT id INTO asset_type_id FROM asset_type WHERE asset_code = asset_code_var AND denom = asset_denom_var LIMIT 1;
+    IF asset_type_id IS NULL THEN
         RAISE EXCEPTION 'Cannot create account with unknown asset type';
     END IF;
-    INSERT INTO account VALUES (owner_id, asset_type, asset_type_id, 'credit_normal');
+    INSERT INTO account(owner_id, asset_type, account_type) VALUES (owner_id, asset_type_id, 'credit_normal');
 END;
 $$ LANGUAGE plpgsql;
