e087955dd8176dc29fad23a73bb493c95a5c3393
Mate Antunovic <just.mate.antunovic@gmail.com>
2018-01-16 10:24:04 -0500

    Add logging in

--- vaso/src/api.ts
+++ vaso/src/api.ts
@@ -1,19 +1,43 @@
 import {Session} from './session';
 
 export class Api {
+    session: Session|null = null;
+
     asyncLogInUser(email_address: string, password: string): Promise<Session> {
-        let login_promise = fetch('http://localhost:3000/users/register/', {
-            method: 'PUT',
+        let login_promise = this.makeRequest('http://localhost:3000/users/log_in/', 'PUT', {
+            email_address: email_address,
+            password: password
+        });
+
+        let parent = this;
+
+        return login_promise
+            .then((response: Response) => response.json())
+            .then((json: any) => {
+                return Promise.resolve(Session.parse(json));
+            })
+            .then((session: Session) => {
+                document.cookie = `session_token=${session.session_token}`;
+                return session;
+            });
+    }
+
+    private makeRequest(url: string, method: string, jsonBody?: any|null): Promise<Response> {
+        let params = {
+            method: method,
             headers: {
                 'Accept': 'application/json, text/plain, */*',
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({email_address: email_address, password: password})
-        });
+            body: undefined
+        };
 
-        return login_promise.then((response: Response) => response.json())
-            .then((json: any) => {
-                return Promise.resolve(Session.parse(json));
-            });
+        if (jsonBody && typeof jsonBody !== 'string') {
+            let bodyString: string;
+            bodyString = JSON.stringify(jsonBody);
+            params.body = bodyString;
+        }
+
+        return fetch(url, params);
     }
 }
