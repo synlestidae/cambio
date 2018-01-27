1be622dee01cd9f5ab20694a82230fb872823e69
Mate Antunovic <just.mate.antunovic@gmail.com>
2018-01-26 20:33:43 -0500

    Break everything

--- casa/src/domain/decrypt_error.rs
+++ casa/src/domain/decrypt_error.rs
@@ -1,6 +1,7 @@
 use std::string::FromUtf8Error;
 use crypto::symmetriccipher::SymmetricCipherError;
 
+#[derive(Debug)]
 pub enum DecryptError {
     FromUtf8Error(FromUtf8Error),
     SymmetricCipherError(SymmetricCipherError),
--- casa/src/domain/ethereum_account_details.rs
+++ casa/src/domain/ethereum_account_details.rs
@@ -9,6 +9,9 @@ use crypto::symmetriccipher::SynchronousStreamCipher;
 use crypto::{ symmetriccipher, buffer, blockmodes };
 use crypto::symmetriccipher::Decryptor;
 use crypto::buffer::{ ReadBuffer, WriteBuffer };
+use crypto::blockmodes::PaddingProcessor;
+use crypto::symmetriccipher::BlockDecryptor;
+use crypto::blockmodes::EcbDecryptor;
 use rand::{OsRng, Rng};
 use std::iter;
 use std;
@@ -28,15 +31,17 @@ impl EthereumAccountDetails {
         sha.input_str(&private_key);
         let hash = sha.result_str();
 
-        let encrypted_private_key = encrypt_string(&private_key, &password);
-        let encrypted_password_base64 = encode(&encrypted_private_key);
+        let encrypted_private_key = encrypt_string(&password, &private_key);
+        println!("Encoding {:?}", encrypted_private_key);
+        let encrypted_password_base64 = (&encrypted_private_key);
+
 
         drop(password);
         drop(private_key);
 
         Self {
             address: address.to_owned(),
-            encrypted_private_key_base64: encode(&encrypted_password_base64),
+            encrypted_private_key_base64: encode(encrypted_password_base64),
             private_key_sha256_hash: hash
         }
     }
@@ -44,6 +49,7 @@ impl EthereumAccountDetails {
     pub fn decrypt_private_key(&self, password: String) -> Result<String, DecryptError> {
         let mut sha = Sha256::new();
         let data = decode(&self.encrypted_private_key_base64).unwrap();
+        println!("Decoding {:?}", data);
         let private_key_string: String = try!(decrypt_string(&password, data));
         sha.input_str(&private_key_string);
         let hash = sha.result_str();
@@ -55,28 +61,41 @@ impl EthereumAccountDetails {
 }
 
 fn encrypt_string(password: &str, string: &str) -> Vec<u8> {
-        let mut cipher = 
-            aes::ecb_encryptor(KeySize::KeySize128, password.as_bytes(), blockmodes::PkcsPadding);
+        let mut cipher = aes::ecb_encryptor(KeySize::KeySize128, 
+            password.as_bytes(), 
+            blockmodes::PkcsPadding);
         let mut buffer_vec = Vec::new();
         buffer_vec.resize(512, 0);
+        println!("Plain! {:?}", string.as_bytes());
         let mut read_buffer = buffer::RefReadBuffer::new(&string.as_bytes());
         let mut write_buffer = buffer::RefWriteBuffer::new(&mut buffer_vec);
-        unimplemented!()
+        let mut final_result = Vec::new();
+
+        loop {
+            let result = cipher.encrypt(&mut read_buffer, 
+                &mut write_buffer, false).expect("Bad read read");
+            let mut result_reader = write_buffer.take_read_buffer();
+            let result_bytes = result_reader.take_remaining();
+            for &b in result_bytes.iter() {
+                final_result.push(b);
+            }
+            match result {
+                buffer::BufferResult::BufferUnderflow => {
+                    break;
+                },
+                buffer::BufferResult::BufferOverflow => { 
+                }
+            }
+        }
+
+        final_result
 }
 
-fn decrypt_string(password: &str, data: Vec<u8>) -> Result<String, DecryptError> {
+fn decrypt_string(password: &str, mut data: Vec<u8>) -> Result<String, DecryptError> {
     let mut decryptor = aes::ecb_decryptor(
             aes::KeySize::KeySize256,
             password.as_bytes(),
             blockmodes::PkcsPadding);
-
-    match String::from_utf8(do_decrypt_read(decryptor, &mut data)) {
-        Ok(string) => Ok(string),
-        Err(error) => Err(DecryptError::FromUtf8Error(error))
-    }
-}
-
-fn do_decrypt_read<T: Decryptor>(decryptor: Box<T>, data: &mut[u8]) -> Vec<u8> {
     let mut buffer_vec = Vec::new();
     buffer_vec.resize(512, 0);
     let mut read_buffer = buffer::RefReadBuffer::new(&mut data);
@@ -88,7 +107,6 @@ fn do_decrypt_read<T: Decryptor>(decryptor: Box<T>, data: &mut[u8]) -> Vec<u8> {
             &mut write_buffer, false).expect("Bad read read");
         let mut result_reader = write_buffer.take_read_buffer();
         let result_bytes = result_reader.take_remaining();
-        println!("Byte boi {}", result_bytes.len());
         for &b in result_bytes.iter() {
             final_result.push(b);
         }
@@ -101,5 +119,7 @@ fn do_decrypt_read<T: Decryptor>(decryptor: Box<T>, data: &mut[u8]) -> Vec<u8> {
         }
     }
 
-    final_result
+    println!("Plain! {:?}", final_result);
+
+    String::from_utf8(final_result).map_err(|error| DecryptError::FromUtf8Error(error))
 }
