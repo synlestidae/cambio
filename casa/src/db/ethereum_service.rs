2062b74a6c99b1af9913114ddb52e3dd01dd454d
Mate Antunovic <just.mate.antunovic@gmail.com>
2018-01-31 11:01:26 -0500

    Add web3 back to main Rust project

--- casa/src/db/ethereum_service.rs
+++ casa/src/db/ethereum_service.rs
@@ -1,6 +1,9 @@
 use db::{PostgresHelper, PostgresHelperError};
 use chrono::prelude::*;
-use domain::{Order, OrderSettlement, Id, EthereumAccountDetails};
+use domain::{Order, OrderSettlement, Id, EthereumAccountDetails, EthereumOutboundTransaction};
+use web3;
+use web3::futures::Future;
+use hex;
 
 #[derive(Clone)]
 pub struct EthereumService<T: PostgresHelper> {
@@ -10,11 +13,23 @@ impl<T: PostgresHelper> EthereumService<T> {
     pub fn register_transaction(&mut self, 
         account: &EthereumAccountDetails, 
         password: String) -> Result<EthereumOutboundTransaction, PostgresHelperError> {
+
         let private_key = account.decrypt_private_key(password).unwrap();
+        let (_eloop, http) = web3::transports::Http::new("http://localhost:8545").unwrap();
+        let web3 = web3::Web3::new(http);
+        let mut address_bytes: [u8; 20] = [0; 20];
+        let hex_addr = "A990F82d33Fd19C3872dc12c588A66224b9330A6";
+        for (i, b) in hex::decode(hex_addr).unwrap().into_iter().enumerate() {
+            address_bytes[i] = b;
+        }
+        let balance = web3.eth().balance(web3::types::H160(address_bytes), None).wait().unwrap();
         unimplemented!()
     }
 
-    pub get_gas_price_szabo_(&mut self) -> u64 {
+    pub fn get_gas_price_szabo(&mut self) -> Result<u64, PostgresHelperError> {
+        let (_eloop, http) = web3::transports::Http::new("http://localhost:8545").unwrap();
+        let web3 = web3::Web3::new(http);
+        web3.eth().gas_price().wait().unwrap();
         unimplemented!()
     }
 }
--- casa/src/main.rs
+++ casa/src/main.rs
@@ -35,6 +35,9 @@ extern crate openssl;
 extern crate crypto;
 extern crate rand;
 extern crate base64;
+extern crate hex;
+
+extern crate web3;
 
 mod db;
 mod domain;
@@ -58,7 +61,8 @@ use time::PreciseTime;
 use iron_cors::CorsMiddleware;
 
 fn main() {
-    env_logger::init().expect("Could not start logger");
+    use web3::futures::Future;
+    /*env_logger::init().expect("Could not start logger");
     let middleware = CorsMiddleware::with_allow_any();
     let helper =
         PostgresHelperImpl::new_from_conn_str("postgres://mate@localhost:5432/cambio_test");
@@ -68,5 +72,15 @@ fn main() {
     let mut chain = iron::Chain::new(router);
     chain.link_around(middleware);
     debug!("Booting up HTTP server");
-    Iron::new(chain).http("localhost:3000").unwrap();
+    Iron::new(chain).http("localhost:3000").unwrap();*/
+
+    let (_eloop, http) = web3::transports::Http::new("http://localhost:8545").unwrap();
+    let web3 = web3::Web3::new(http);
+    let mut address_bytes: [u8; 20] = [0; 20];
+    let hex_addr = "A990F82d33Fd19C3872dc12c588A66224b9330A6";
+    for (i, b) in hex::decode(hex_addr).unwrap().into_iter().enumerate() {
+        address_bytes[i] = b;
+    }
+    let balance = web3.eth().balance(web3::types::H160(address_bytes), None).wait().unwrap();
+    println!("We got: {}", balance);
 }
