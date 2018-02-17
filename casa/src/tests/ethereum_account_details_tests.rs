34e294ccf724d83d013258871e4ed50f86be9308
Mate Antunovic <just.mate.antunovic@gmail.com>
2018-02-17 09:45:35 -0500

    Remove old tests

--- casa/src/tests/ethereum_account_details_tests.rs
+++
@@ -1,1 +0,0 @@
-use domain::EthAccount;
--- casa/src/tests/mod.rs
+++ casa/src/tests/mod.rs
@@ -1,9 +1,3 @@
-mod user_repository_tests;
 mod test_utils;
-mod account_service_tests;
-mod payment_repository_tests;
-mod order_service_tests;
-mod ethereum_account_details_tests;
-mod ethereum_service_tests;
 
 pub use self::test_utils::get_db_helper;
