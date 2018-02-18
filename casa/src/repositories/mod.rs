13f64f75d07e3597aba1372a26de1a28b07130ab
Mate Antunovic <just.mate.antunovic@gmail.com>
2018-02-18 14:49:38 -0500

    Add payment repo

--- casa/src/repositories/mod.rs
+++ casa/src/repositories/mod.rs
@@ -4,6 +4,7 @@ mod user_repository;
 mod order_repository;
 mod settlement_repository;
 mod media_repository;
+mod payment_repository;
 
 pub use self::account_repository::AccountRepository;
 pub use self::session_repository::SessionRepository;
@@ -11,3 +12,4 @@ pub use self::user_repository::UserRepository;
 pub use self::order_repository::OrderRepository;
 pub use self::settlement_repository::SettlementRepository;
 pub use self::media_repository::MediaRepository;
+pub use self::payment_repository::PaymentRepository;
