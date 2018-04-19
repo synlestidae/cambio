286a908d236ac5f2dd341b7b8a28bf4ef1f4986e
Mate Antunovic <just.mate.antunovic@gmail.com>
2018-04-19 14:27:49 +0200

    Make navbar links interactive

--- vaso/src/navbar_component.tsx
+++ vaso/src/navbar_component.tsx
@@ -1,9 +1,24 @@
 import * as React from "react";
 
-const links = ['Home', 'Board', 'Buy', 'Sell', 'My account'];
+interface Link {
+    title: string,
+    url: string
+}
+
+const links = [
+    {title: 'Home', url: ''},
+    {title: 'Board', url: '#board'},
+    {title: 'My account', url: '#my_account'}
+];
+
+function makeLink(link: Link, i: number) {
+    return <div key={i} className="nav-element clickable">
+        <a href={link.url}>{link.title}</a>
+    </div>;
+}
 
 export function NavbarComponent() {
-    let navLinks = links.map((link: string, i: number) => (<div key={i} className="nav-element clickable">{link}</div>));
+    let navLinks = links.map(makeLink);
     return <div id="top-nav">
       <div className="nav-logo">
         CAMBIO Ltd.
