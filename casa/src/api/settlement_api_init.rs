64d54b3533a48e8ed44861f65b5660d3c3e738f5
Mate Antunovic <just.mate.antunovic@gmail.com>
2018-06-15 00:11:27 +0200

    Add the settlement credentials endpoint

--- casa/src/api/settlement_api_init.rs
+++ casa/src/api/settlement_api_init.rs
@@ -2,12 +2,22 @@ use api::ApiInit;
 use iron::headers::{Authorization, Bearer, Cookie};
 use iron::request::Request;
 use router::Router;
+use db;
 
-pub struct SettlementApiInit {
+pub struct SettlementApiInit<H: db::PostgresHelper> {
+    db: H
 }
 
-impl ApiInit for SettlementApiInit {
+impl<H: db::PostgresHelper> ApiInit for SettlementApiInit<H> {
     fn init_api(&mut self, router: &mut Router) {
+        router.post(
+            "/orders/:id/eth_credentials",
+            move |r: &mut Request| {
+                unimplemented!()
+            },
+            "post_settlement_eth_auth",
+        );
+
         unimplemented!()
     }
 }
