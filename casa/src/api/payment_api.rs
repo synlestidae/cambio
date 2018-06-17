eb40181ba2b048d401220f606f412258013ea60d
Mate Antunovic <just.mate.antunovic@gmail.com>
2018-06-18 00:54:02 +0200

    Fix account payment

--- casa/src/api/payment_api.rs
+++ casa/src/api/payment_api.rs
@@ -1,7 +1,8 @@
 use db::PostgresHelper;
-use domain::{Payment, User, Account};
+use domain::{Payment, User, Account, AccountSet};
 use iron::response::Response;
 use repository::Readable;
+use iron;
 
 pub struct PaymentApi<H: PostgresHelper + 'static> {
     db: H
@@ -21,8 +22,15 @@ impl<H: PostgresHelper + 'static> PaymentApi<H> {
             SELECT credit_account_from_payment($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);
         ";
         // TODO this is distinctly wrong
-        let account: Account = user.owner_id.unwrap().get(&mut self.db).unwrap();
-        let account_id = account.id;
+        let accounts: Vec<Account> = match user.owner_id.unwrap().get_vec(&mut self.db) {
+            Ok(accounts) => accounts,
+            Err(err) => return err.into()
+        };
+        let account_set: AccountSet = match AccountSet::from(accounts) {
+            Ok(accounts) => accounts,
+            Err(err) => return err.into()
+        };
+        let account_id = account_set.nzd_wallet();
         match self.db.execute(CREDIT_FUNC, &[
             &user.id,
             &user.email_address,
@@ -33,7 +41,7 @@ impl<H: PostgresHelper + 'static> PaymentApi<H> {
             &payment.user_credit,
             &"Test credit of {} from user."
         ]) {
-            Ok(_) => unimplemented!(),
+            Ok(_) => iron::response::Response::with((iron::status::Status::Ok, format!(""))), //unimplemented!(),
             Err(err) => err.into(),
         }
     }
