007c26835eaa64ee86405d8de587e10a298eefbc
Mate Antunovic <just.mate.antunovic@gmail.com>
2018-06-23 13:50:23 +0200

    Add ToSql and FromSql for IdentificationCode

--- casa/src/domain/identifier_code.rs
+++ casa/src/domain/identifier_code.rs
@@ -1,2 +1,37 @@
-#[derive(Eq, PartialEq, ToSql, FromSql, Debug, Clone, Deserialize, Serialize)]
+use postgres::types::{ToSql, FromSql, Type, IsNull};
+use std::error::Error;
+
+#[derive(Eq, PartialEq, Debug, Clone, Deserialize, Serialize)]
 pub struct IdentifierCode(pub String);
+
+impl FromSql for IdentifierCode {
+    fn from_sql(ty: &Type, raw: &[u8]) -> Result<Self, Box<Error + 'static + Send + Sync>> {
+        match String::from_utf8(raw.iter().map(|&x| x).collect()) {
+            Ok(s) => Ok(IdentifierCode(s)),
+            Err(err) => Err(Box::new(err))
+        }
+    }
+
+    fn accepts(ty: &Type) -> bool {
+        true
+    }
+}
+
+impl ToSql for IdentifierCode {
+	fn to_sql(&self, ty: &Type, out: &mut Vec<u8>) -> Result<IsNull, Box<Error + 'static + Send + Sync>> {
+        let mut bytes = self.0.clone().bytes().collect();
+		out.append(&mut bytes);
+		Ok(IsNull::No)
+	}
+
+	fn accepts(ty: &Type) -> bool {
+		true
+	}
+
+	fn to_sql_checked(&self, ty: &Type, out: &mut Vec<u8>) 
+		-> Result<IsNull, Box<Error + 'static + Send + Sync>> {
+        let mut bytes = self.0.clone().bytes().collect();
+		out.append(&mut bytes);
+        Ok(IsNull::No)
+	}
+}
