de4a3db1fe68d4a02c3fae33962b6cc272d4a76f
Mate Antunovic <just.mate.antunovic@gmail.com>
2018-07-02 05:33:19 +1200

    Change FormElem instances to include data (values)

--- vaso/src/signup_form.tsx
+++ vaso/src/signup_form.tsx
@@ -15,18 +15,22 @@ export interface FormElem {
     validate(val: string): string|null;
     formType?: 'option' | 'password' | 'text';
     name?: string;
+    value: string;
+    disabled?: boolean;
 }
 
 export class OptionElem implements FormElem {
     public options: string[];
     public label: string;
     public field: string;
+    public value: string;
     public readonly formType = 'option';
 
-    constructor(field: string, label: string, options: string[], msg: string) {
+    constructor(field: string, label: string, options: string[], msg: string, value: string) {
         this.field = field;
         this.label = label;
         this.options = options;
+        this.value = value;
     }
 
     public validate(value: string): string|null {
@@ -38,6 +42,7 @@ export class OptionElem implements FormElem {
 
 export function buildForm(props: SignupState & PartialSignupFormProps): FormElem[] {
     let elems: FormElem[] = [];
+
     const nonEmpty = function(msg: string) {
         return (val: string) => Boolean(val)? null : msg;
     };
@@ -47,21 +52,24 @@ export function buildForm(props: SignupState & PartialSignupFormProps): FormElem
                 label: 'Email address', 
                 field: 'email_address', 
                 validate: nonEmpty('Enter your email address.'),
-                name: 'email'
+                name: 'email',
+                value: props.loginInfo.email_address
             },
             {
                 label: 'Password', 
                 field: 'password', 
                 validate: (val: string) => val.length >= 8? null : 'Enter a password of at least 8 characters.', 
                 formType: 'password',
-                name: 'password'
+                name: 'password',
+                value: props.loginInfo.password
             },
             {
                 label: 'Confirm password', 
                 field: 'passwordConfirm', 
                 validate: () => props.loginInfo.password === props.loginInfo.passwordConfirm? null : 'Passwords must match.',
                 formType: 'password',
-                name: 'password'
+                name: 'password',
+                value: props.loginInfo.passwordConfirm
             }
         ];
     } else if (props.form_state == 'PersonalInfo') {
@@ -70,48 +78,67 @@ export function buildForm(props: SignupState & PartialSignupFormProps): FormElem
                 label: 'First name(s)',
                 field: 'first_names',
                 validate: nonEmpty('Enter your given names (including middle names).'),
-                name: "given-name"
+                name: "given-name",
+                value: props.info.first_names
             },
             {
                 label: 'Family name',
                 field: 'family_names',
                 validate: nonEmpty('Enter your last or family name.'),
-                name: "family-name"
+                name: "family-name",
+                value: props.info.family_name
             },
             {
                 label: 'Address line 1',
                 field: 'address_line_1',
                 validate: nonEmpty('Enter the top line you would put on a letter.'),
-                name: 'address-line1'
+                name: 'address-line1',
+                value: props.info.address_line_1
             },
             {
                 label: 'Address line 2',
                 field: 'address_line_2',
                 validate: () => null,
-                name: 'address-line2'
+                name: 'address-line2',
+                value: props.info.address_line_2
             },
             {
                 label: 'Post code',
                 field: 'post_code',
                 validate: nonEmpty('Enter your address\' four-digit NZ post code.'),
-                name: 'postal-code'
+                name: 'postal-code',
+                value: props.info.post_code
             },
             {
                 label: 'City or town',
                 field: 'city', 
                 validate: nonEmpty('Enter the place name of this address'),
-                name: 'city'
+                name: 'city',
+                value: props.info.city
             },
             {
                 label: 'Country',
                 field: 'country',
-                validate: () => null
+                validate: () => null,
+                value: 'New Zealand',
+                disabled: true
             },
         ];
     } else if (props.form_state == 'Identification') {
         elems = [
-            new OptionElem('id_type', 'Type of NZ identification', ['Passport_NZ', 'DriversLicence_NZ'], 'Please choose a type of ID'),
-            {label: 'ID Number', field: 'id_number', validate: nonEmpty('Enter the ID number')}
+            new OptionElem(
+                'id_type', 
+                'Type of NZ identification', 
+                ['Passport_NZ', 'DriversLicence_NZ'],
+                'Please choose a type of ID',
+                props.identification.id_type
+            ),
+            {
+                label: 'ID Number', 
+                field: 'id_number', 
+                validate: nonEmpty('Enter the ID number'),
+                value: props.identification.id_number
+            }
         ];
     }
     return elems;
@@ -123,29 +150,19 @@ export function SignupForm(elems: FormElem[], actions: ActionCreators, props: Si
 
 function makeForm(elems: FormElem[], actions: ActionCreators, props: SignupState): JSX.Element[] {
     let jsxElements = [];
-    let value: any;
-    if (props.form_state === 'LoginInfo') {
-        value = props.loginInfo; 
-    } else if (props.form_state === 'PersonalInfo') {
-        value = props.info;
-    } else if (props.form_state === 'Identification') {
-        value = props.identification;
-    } else {
-        throw new Error(`Form state ${props.form_state} yet implemented`);
-    }
     let i = 0;
     for (let e of elems) {
-        jsxElements.push(getFormElement(e, actions, props.dirtyFields.has(e.field), value));
+        jsxElements.push(getFormElement(e, actions, props.dirtyFields.has(e.field)));
         i++;
     }
     return jsxElements;
 }
 
-function getFormElement(elem: FormElem, actions: ActionCreators, isDirty: boolean, value: any): JSX.Element {
-    let inputValue = value[elem.field];
+function getFormElement(elem: FormElem, actions: ActionCreators, isDirty: boolean): JSX.Element {
+    let inputValue = elem.value;
     const handleBlur = function() {
         actions.clearDirtyValue(elem.field);
-        if (elem.validate(value[elem.field]) !== null) {
+        if (elem.validate(inputValue) !== null) {
             actions.addDirtyValue(elem.field);
             console.log('Blur!', elem, inputValue);
         }
@@ -170,12 +187,14 @@ function getFormElement(elem: FormElem, actions: ActionCreators, isDirty: boolea
     } else {
         return (<div className="form-row">
               <label className="sr-only">{elem.label}</label>
-              <input type={elem.formType === 'password'? 'password' : 'text'}
+              <input 
+                type={elem.formType === 'password'? 'password' : 'text'}
                 id={'signup_' + elem.field} 
                 className="form-control" 
-                value={value[elem.field]} 
+                value={inputValue} 
                 placeholder={elem.label}
                 autoComplete={elem.name? 'on' : 'off'}
+                disabled={elem.disabled}
                 name={elem.name || elem.field}
                 onBlur={handleBlur}
                 onFocus={handleFocus}
