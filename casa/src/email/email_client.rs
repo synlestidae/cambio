19d087bcb90695b383360de797738a658c93e331
Mate Antunovic <just.mate.antunovic@gmail.com>
2018-07-23 16:13:11 +1200

    Implement the Send functionality

--- casa/src/email/email_client.rs
+++ casa/src/email/email_client.rs
@@ -1,7 +1,9 @@
 use config::EmailConfig;
 use email::email_message::EmailMessage;
 use email::smtp_response::SMTPResponse;
-use email::smtp_error::SMTPError;
+use email::email_client_error::EmailClientError;
+use lettre::{EmailTransport, SmtpTransport};
+use lettre::smtp::authentication::Credentials;
 
 pub struct EmailClient {
     server_host: String,
@@ -18,7 +20,17 @@ impl EmailClient {
         }
     }
 
-    pub fn send(&self, message: &EmailMessage) -> Result<SMTPResponse, SMTPError> {
-        unimplemented!()
+    pub fn send(&self, message: &EmailMessage) -> Result<SMTPResponse, EmailClientError> {
+        let lettre_email = message.to_lettre_email();
+        let credentials = Credentials::new(self.login.to_string(), self.password.to_string());
+        let mut client = SmtpTransport::simple_builder(&self.server_host)?
+            .build();
+        let lettre_response = client.send(&lettre_email)?;
+        let response = SMTPResponse::from_code(&lettre_response.code);
+        if response.success() {
+            Ok(response)
+        } else {
+            Err(response.into())
+        }
     }
 }
---
+++ casa/src/email/email_client_error.rs
@@ -0,0 +1,27 @@
+use email::smtp_error::SMTPError;
+use email::smtp_response::SMTPResponse;
+use lettre::smtp::error::Error as LettreError;
+
+pub enum EmailClientError {
+    SMTPError(SMTPError),
+    SMTPFail(SMTPResponse),
+    Net
+}
+
+impl From<LettreError> for EmailClientError{
+    fn from(err: LettreError) -> Self {
+        EmailClientError::SMTPError(err.into())
+    }
+}
+
+impl From<SMTPResponse> for EmailClientError{
+    fn from(err: SMTPResponse) -> Self {
+        EmailClientError::SMTPFail(err)
+    }
+}
+
+impl From<SMTPError> for EmailClientError{
+    fn from(err: SMTPError) -> Self {
+        EmailClientError::SMTPError(err)
+    }
+}
