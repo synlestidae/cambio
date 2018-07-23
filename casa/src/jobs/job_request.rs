2bae9b30d6eb5e1f21c519ca7cee0628a5d5e7ce
Mate Antunovic <just.mate.antunovic@gmail.com>
2018-07-24 09:16:00 +1200

    Add email functionality to JobLoop

--- casa/src/jobs/job_request.rs
+++ casa/src/jobs/job_request.rs
@@ -1,9 +1,11 @@
 use domain::OrderSettlementId;
+use jobs::EmailRequest;
 use std::marker::Sync;
 
 #[derive(Debug)]
 pub enum JobRequest {
     BeginSettlement(OrderSettlementId, String),
+    SendEmail(EmailRequest)
 }
 
 unsafe impl Sync for JobRequest {}
