e0dfbc529a89d96956b74ad664c4956b85f8ddc5
Mate Antunovic <just.mate.antunovic@gmail.com>
2018-07-23 15:40:24 +1200

    Response and error types for SMTP (#1)

--- casa/src/email/smtp_error.rs
+++ casa/src/email/smtp_error.rs
@@ -1,1 +1,19 @@
-pub struct SMTPError;
+use lettre::smtp::error::Error as LettreError;
+
+#[derive(Debug)]
+pub struct SMTPError {
+    can_retry: bool,
+}
+
+impl From<LettreError> for SMTPError {
+    fn from(err: LettreError) -> Self {
+        let can_retry = match err {
+            LettreError::Permanent(..) => false,
+            LettreError::Client(..) => false,
+            _ => true
+        };
+        Self {
+            can_retry: can_retry
+        }
+    }
+}
--- casa/src/email/smtp_response.rs
+++ casa/src/email/smtp_response.rs
@@ -1,11 +1,15 @@
 use lettre::smtp::response::Code;
+use lettre::smtp::response::Severity;
 
+#[derive(Debug)]
 pub struct SMTPResponse {
     was_successful: bool
 }
 
 impl SMTPResponse {
     fn from_code(code: &Code) -> Self {
-        unimplemented!()
+        Self {
+            was_successful: code.severity == Severity::PositiveCompletion
+        }
     }
 }
