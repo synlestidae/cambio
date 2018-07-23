65d327190cd7be9a0bf35cc27979b2863ebcbdf9
Mate Antunovic <just.mate.antunovic@gmail.com>
2018-07-24 09:31:54 +1200

    Email config accessible in job loop (#1)

--- casa/src/jobs/email_request.rs
+++ casa/src/jobs/email_request.rs
@@ -1,9 +1,19 @@
 use lettre::EmailAddress;
+//use email::ToEmailMessage;
+use email::*;
 
 #[derive(Debug)]
 pub enum EmailRequest {
     ConfirmationCode {
-        email_address: EmailAddress,
+        from: EmailAddress,
+        to: EmailAddress,
+        name: String,
         confirmation_code: String 
+    }
+}
+
+impl EmailRequest {
+    pub fn to_email(&self) -> EmailMessage {
+        unimplemented!()
     }
 }
