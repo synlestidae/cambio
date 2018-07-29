5412648a70a108d4dd809c324c75d12488d6b4e1
Mate Antunovic <just.mate.antunovic@gmail.com>
2018-07-29 15:41:44 +1200

    Fix syntax on order modal (#8)

--- vaso/src/new_order_component.tsx
+++ vaso/src/new_order_component.tsx
@@ -15,22 +15,24 @@ export function NewOrderComponent(props: NewOrderComponentProps) {
     const onSubmit = () => props.actions.startNewOrderConfirm();
     const onCancel = () => props.actions.cancelNewOrder();
     return <div className="modal order-modal">
-		<div className="form-group">
-			<label>ETH to buy</label>
-			<input className="form-control" type="number" step="0.001" min="0" max="1000" onChange={onChangeBuyUnits}>
-			</input>
-		</div>
-		<div className="form-group">
-			<label>NZD to sell</label>
-			<input max="5000" min="0" step="0.01" type="number" className="form-control" onChange={onChangeSellUnits}>
-		</div>
-		<div className="form-group">
-			<label>ETH Price</label>
-			<input type="text" disabled className="form-control" value="3863.8462">
-		</div>
-		<div className="form-group">
-			<button className="non-touching-button btn btn-primary" onClick={onSubmit}>Cancel</button>
-			<button className="non-touching-button btn btn-primary" onClick={onCancel}>Submit order</button>
-		</div>
-	</div>;
+        <div className="form-group">
+          <label>ETH to buy</label>
+          <input className="form-control" type="number" step="0.001" min="0" max="1000" onChange={onChangeBuyUnits}>
+          </input>
+        </div>
+        <div className="form-group">
+          <label>NZD to sell</label>
+          <input max="5000" min="0" step="0.01" type="number" className="form-control" onChange={onChangeSellUnits}>
+          </input>
+        </div>
+        <div className="form-group">
+          <label>ETH Price</label>
+          <input type="text" disabled className="form-control" value="3863.8462">
+          </input>
+        </div>
+        <div className="form-group">
+          <button className="non-touching-button btn btn-primary" onClick={onSubmit}>Cancel</button>
+          <button className="non-touching-button btn btn-primary" onClick={onCancel}>Submit order</button>
+        </div>
+      </div>;
 }
