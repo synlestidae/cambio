8051631c5482dabaf0f319a88a9594a401ba21c1
Mate Antunovic <just.mate.antunovic@gmail.com>
2018-08-21 13:46:48 +1200

    Add logging to clerks

--- casa/src/clerks/ethereum_clerk.rs
+++ casa/src/clerks/ethereum_clerk.rs
@@ -24,10 +24,14 @@ pub struct EthereumClerk<T: Transport> {
 
 impl<T: Transport> EthereumClerk<T> {
     fn subscribe_address(&mut self, address: ByteAddress) {
+        info!("Subscribing to address {:?}", address);
+
         let bus_copy = self.bus.clone();
         let filter = FilterBuilder::default()
             .address(vec![address.into()])
             .build();
+
+        info!("Creating filter for {:?}", address);
 
         let filter_result = self.web3
             .eth_filter()
@@ -40,19 +44,24 @@ impl<T: Transport> EthereumClerk<T> {
     }
 
     fn handle_subscribe_address_logs(bus: Bus, eth: Eth<T>, logs: Vec<Log>) {
+        info!("Got {} logs. Getting current block", logs.len());
         let current_block = eth.block_number().wait().unwrap();
+        info!("Current block is {}", current_block);
         for log in logs.into_iter() {
             let (tx_id, block) = match (log.block_number, log.transaction_index, log.transaction_hash, log.block_number) {
                 (_, _, Some(hash), Some(bn)) => (TransactionId::Hash(hash), bn.low_u64()),
                 _ => continue
             };
             let block = eth.block(BlockId::Number(BlockNumber::Number(block))).wait().unwrap();
+            info!("Getting transaction from log {:?}", tx_id);
             let transaction = eth.transaction(tx_id).wait().unwrap().unwrap();
             let transfer = EthTransfer::from(&transaction, &block).unwrap();
             let block_number = transfer.block_number; 
             if (current_block - block_number).low_u64() >= 11 {
+                info!("Transfer is safely confirmed. Broadcasting");
                 bus.send(&transfer, &EthereumEventType::TransferConfirmed); 
             } else {
+                info!("Transfer requires confirmation");
                 bus.send(&transfer, &EthereumEventType::WaitTransferConfirmation); 
             }
         }
--- casa/src/clerks/ethereum_settlement_clerk.rs
+++ casa/src/clerks/ethereum_settlement_clerk.rs
@@ -15,10 +15,15 @@ pub struct EthereumSettlementClerk<C: GenericConnection> {
 
 impl<C: GenericConnection> EthereumSettlementClerk<C> {
     pub fn handle_eth_transfer(&mut self, eth_transfer: EthTransfer) -> Result<(), CambioError> {
+        info!("Transfer of {} from addr {} to {} confirmed", eth_transfer.value, 
+              eth_transfer.from, 
+              eth_transfer.to);
         let addr: ByteAddress = eth_transfer.from.clone().into();
+        info!("Checking settlements related to this transfer");
         let settlements: Vec<OrderSettlement> = addr.get_vec(&mut self.db)?;
-
+        info!("Got {} settlements", settlements.len());
         for settlement in settlements.into_iter() {
+            info!("Beginning settlement transaction");
             let mut tx = self.db.transaction()?;
             let criteria: SettlementCriteria = settlement.order_with_criteria().get(&mut tx)?;
             let eth_account = criteria
@@ -30,8 +35,8 @@ impl<C: GenericConnection> EthereumSettlementClerk<C> {
                 .get(&mut tx)?;
 
 
+            info!("Packaging settlement parameters");
             // TODO turn this into some kind of builder
-
             let package = SettlementPackage {
                 settlement_addr: settlement.eth_account.get(&mut tx)?.address.into(),
                 original_order: settlement.original_order.get(&mut tx)?,
@@ -43,7 +48,10 @@ impl<C: GenericConnection> EthereumSettlementClerk<C> {
             };
 
             if package.addresses_match() {
+                info!("Addresses match. Handling settlement now.");
                 self.handle_order_settlement(&mut tx, package);
+            } else {
+                info!("Addresses don't match on transfer");
             }
 
             tx.set_commit();
@@ -54,14 +62,20 @@ impl<C: GenericConnection> EthereumSettlementClerk<C> {
 
     fn handle_order_settlement<D: GenericConnection>(&self, db: &mut D, mut package: SettlementPackage) {
         if !package.can_proceed() {
-            warn!("Settlement  cannot proceed");
+            warn!("Settlement cannot proceed. It may already be settled.");
+            return;
         }
         if !package.completes_settlement() {
-            warn!("Settlement does not fulfill order");
+            warn!("THe Ethereum transfer does not fulfill the settlement");
+            return;
         }
         if !package.is_on_time() {
-            warn!("Settlement was not on time");
+            warn!("Settlement was late. This settlement will be marked as failed.");
+            return;
         }
+
+        // Danger area
+
         package.mark_settled();
         match package.update_fields(db) {
             Ok(_) => {},
@@ -71,6 +85,8 @@ impl<C: GenericConnection> EthereumSettlementClerk<C> {
                 // Settlement will just have to be picked up by routine settlement checker
             }
         }
+
+        // TODO transfer funds from A to B
     }
 }
 
